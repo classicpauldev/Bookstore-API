d8e9c4d8d734d9e0551f083406379c63e1d1fe42
classicpauldev <1337@classicpaul.dev>
2022-08-21 12:00:00 -0700

    Initialize Node.js project with package.json

---
+++ Bookstore-API/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/bookstore';
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Bookstore API!');
+});
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+})
+.catch((err) => {
+  console.error('Failed to connect to MongoDB', err);
+}); 
