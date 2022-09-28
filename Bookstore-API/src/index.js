7717a7c96cf4fb5e03656cf7c4aeb48af021d012
classicpauldev <1337@classicpaul.dev>
2022-09-28 12:01:00 -0700

    Added API versioning support

--- Bookstore-API/src/index.js
+++ Bookstore-API/src/index.js
@@ -64,3 +64,4 @@ mongoose.connect(MONGODB_URI, {
 // Commit: Fixed cross-browser compatibility issues on 2022-09-25 12:01:00
 // Commit: Added user activity tracking on 2022-09-26 12:01:00
 // Commit: Fixed session management bugs on 2022-09-27 12:01:00
+// Commit: Added API versioning support on 2022-09-28 12:01:00
